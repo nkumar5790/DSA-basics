7796fb48afa95c8a84e96a033bae2a3eb8a330b5
Nishant Kumar <nkumar@demandbase.com>
2024-04-15 08:37:44 +0530

    udpating main file

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
     test_array = [32,54,34,1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     test_target = 3
     test_search = searching.Search(test_array)
-    # print(test_search.linear_search(test_target))
+    print(test_search.linear_search(test_target))
     print(test_search.binary_search(test_target))
